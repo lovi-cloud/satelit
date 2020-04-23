29a4395f157b94e76c6ca8f44c92efba6943b314
whywaita <whywaita@whywrite.it>
2020-04-23 11:52:57 +0900

    implement IsAttached

--- pkg/europa/europa.go
+++ pkg/europa/europa.go
@@ -10,12 +10,12 @@ type Europa interface {
 	CreateVolume(ctx context.Context, name uuid.UUID, capacity int) (*Volume, error)
 	DeleteVolume(ctx context.Context, name uuid.UUID) error
 	ListVolume(ctx context.Context) ([]Volume, error)
-	AttachVolume(ctx context.Context, name uuid.UUID, hostname string) error
+	AttachVolume(ctx context.Context, name uuid.UUID, hostname string) (*Volume, error)
+	IsAttached(ctx context.Context, name uuid.UUID) (bool, error)
 }
 
 type Volume struct {
 	ID       string
-	Attached bool
 	HostName string
 	Capacity int // is GB
 }
