8b699434eec27c196d5a43878a76dcc477dd6794
whywaita <whywaita@whywrite.it>
2020-05-21 19:03:57 +0900

    add example that call UploadImage

--- examples/client/main.go
+++ examples/client/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
+
+	"google.golang.org/grpc"
 
 	"github.com/whywaita/satelit/internal/config"
 
-	uuid "github.com/satori/go.uuid"
-
 	pb "github.com/whywaita/satelit/api/satelit"
-	"google.golang.org/grpc"
 )
 
 func init() {
@@ -43,34 +43,94 @@ func run() error {
 		return err
 	}
 
-	u, err := uuid.NewV4()
+	fmt.Println("UploadImage")
+	args := os.Args
+	fmt.Printf("args: %s\n", args)
+	imageFile := args[1]
+	f, err := os.Open(imageFile)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("AddVolume")
-	resp, err := client.AddVolume(ctx, &pb.AddVolumeRequest{
-		Name:         u.String(),
-		CapacityByte: 23,
-	})
+	image, err := UploadImage(ctx, client, f, "whywaita-test-cirros", "cirros")
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%+v\n", resp.Volume)
+	fmt.Printf("%+v\n", image)
 
-	var hostname string
-	for h := range config.GetValue().Teleskop.Endpoints {
-		hostname = h // set last hostname
-	}
-
-	fmt.Println("AttachVolume")
-	_, err = client.AttachVolume(ctx, &pb.AttachVolumeRequest{
-		Id:       resp.Volume.Id,
-		Hostname: hostname,
-	})
-	if err != nil {
-		return err
-	}
+	//fmt.Println("AddVolume")
+	//resp, err := client.AddVolume(ctx, &pb.AddVolumeRequest{
+	//	Name:         u.String(),
+	//	CapacityByte: 23,
+	//})
+	//if err != nil {
+	//	return err
+	//}
+	//fmt.Printf("%+v\n", resp.Volume)
+	//
+	//var hostname string
+	//for h := range config.GetValue().Teleskop.Endpoints {
+	//	hostname = h // set last hostname
+	//}
+	//
+	//fmt.Println("AttachVolume")
+	//_, err = client.AttachVolume(ctx, &pb.AttachVolumeRequest{
+	//	Id:       resp.Volume.Id,
+	//	Hostname: hostname,
+	//})
+	//if err != nil {
+	//	return err
+	//}
 
 	return nil
+}
+
+// UploadImage upload image
+func UploadImage(ctx context.Context, client pb.SatelitClient, src io.Reader, name, description string) (*pb.Image, error) {
+	stream, err := client.UploadImage(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return uploadImage(stream, src, name, description)
+}
+
+func uploadImage(stream pb.Satelit_UploadImageClient, src io.Reader, name, description string) (*pb.Image, error) {
+	meta := &pb.UploadImageRequest{
+		Value: &pb.UploadImageRequest_Meta{
+			Meta: &pb.UploadImageRequestMeta{
+				Name:        name,
+				Description: description,
+			}}}
+	err := stream.Send(meta)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := make([]byte, 1024)
+	for {
+		_, err := src.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		data := &pb.UploadImageRequest{
+			Value: &pb.UploadImageRequest_Chunk{
+				Chunk: &pb.UploadImageRequestChunk{
+					Data: buf}}}
+		err = stream.Send(data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Image, nil
 }
