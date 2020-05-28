7ac89eb5328c5b223e66c52244db7ea7b7331c8e
whywaita <whywaita@whywrite.it>
2020-05-28 19:10:07 +0900

    rename europa.CreateVolume

--- pkg/europa/europa.go
+++ pkg/europa/europa.go
@@ -8,8 +8,8 @@ import (
 
 // Europa is interface of volume operation.
 type Europa interface {
-	CreateVolumeRaw(ctx context.Context, name uuid.UUID, capacity int) (*Volume, error)
-	CreateVolumeImage(ctx context.Context, name uuid.UUID, capacity int, imageID string) (*Volume, error)
+	CreateVolume(ctx context.Context, name uuid.UUID, capacity int) (*Volume, error)
+	CreateVolumeFromImage(ctx context.Context, name uuid.UUID, capacity int, imageID string) (*Volume, error)
 	DeleteVolume(ctx context.Context, id string) error
 	ListVolume(ctx context.Context) ([]Volume, error)
 	GetVolume(ctx context.Context, id string) (*Volume, error)
