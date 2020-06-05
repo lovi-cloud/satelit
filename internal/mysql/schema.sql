420fd3ab7b20887e817bcace1c271fd7828e789c
Yutaka Juba <jyuba_yutaka@cyberagent.co.jp>
2020-06-05 14:22:25 +0900

    Add tables used by ipam

--- internal/mysql/schema.sql
+++ internal/mysql/schema.sql
@@ -19,3 +19,22 @@ CREATE TABLE IF NOT EXISTS image (
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp on update current_timestamp
 );
+
+CREATE TABLE IF NOT EXISTS subnet (
+    uuid BINARY(16) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    network VARCHAR(255) NOT NULL UNIQUE,
+    start VARCHAR(255) NOT NULL,
+    end VARCHAR(255) NOT NULL,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS address (
+    uuid BINARY(16) NOT NULL PRIMARY KEY,
+    ip VARCHAR(255) NOT NULL UNIQUE,
+    subnet_id BINARY(16) NOT NULL,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp,
+    FOREIGN KEY fk_subnet_id(subnet_id) REFERENCES subnet(uuid) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
