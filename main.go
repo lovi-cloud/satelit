6403b9f686ae7f7d088f3b93900e76c951ee8f7d
Yutaka Juba <jyuba_yutaka@cyberagent.co.jp>
2020-06-22 20:40:44 +0900

    Add datastore API invocation

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/whywaita/satelit/internal/logger"
+	"golang.org/x/sync/errgroup"
+
 	"github.com/whywaita/satelit/cmd"
 )
 
@@ -22,6 +25,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Run
-	os.Exit(app.Run())
+	datastore, err := cmd.NewSatelitDatastore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var eg errgroup.Group
+	eg.Go(func() error {
+		return app.Run()
+	})
+	eg.Go(func() error {
+		return datastore.Run()
+	})
+	if err := eg.Wait(); err != nil {
+		logger.Logger.Error(fmt.Sprintf("%+v", err))
+		os.Exit(1)
+	}
 }
