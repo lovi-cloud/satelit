2e49228c6830784eeae0f4eba9e3787fc28336dd
whywaita <whywaita@whywrite.it>
2020-06-25 21:55:29 +0900

    add test for AddVolume

--- pkg/api/server_test.go
+++ pkg/api/server_test.go
@@ -1,13 +1,22 @@
 package api
 
 import (
+	"context"
+	"log"
+	"net"
 	"testing"
 
-	"github.com/whywaita/satelit/pkg/ipam/ipam"
+	"github.com/go-test/deep"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+
+	pb "github.com/whywaita/satelit/api/satelit"
 
 	datastoreMemory "github.com/whywaita/satelit/pkg/datastore/memory"
 	europaMemory "github.com/whywaita/satelit/pkg/europa/memory"
 	ganymedeMemory "github.com/whywaita/satelit/pkg/ganymede/memory"
+	"github.com/whywaita/satelit/pkg/ipam/ipam"
 )
 
 func TestSanitizeImageSize(t *testing.T) {
@@ -43,5 +52,67 @@ func NewMemorySatelit() *SatelitServer {
 		IPAM:      ipamBackend,
 		Datastore: ds,
 		Ganymede:  ganymede,
+	}
+}
+
+const bufSize = 1024 * 1024
+
+var lis *bufconn.Listener
+
+func init() {
+	server := NewMemorySatelit()
+
+	lis = bufconn.Listen(bufSize)
+	s := grpc.NewServer()
+	pb.RegisterSatelitServer(s, server)
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
+func bufDialer(ctx context.Context, address string) (net.Conn, error) {
+	return lis.Dial()
+}
+
+func getSatelitClient() (context.Context, pb.SatelitClient, func() error) {
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := pb.NewSatelitClient(conn)
+
+	return ctx, client, conn.Close
+}
+
+const (
+	testVolumeName       = "TEST_VOLUME"
+	testCapacityGigabyte = 8
+	testUUID             = "90dd6cd4-b3e4-47f3-9af5-47f78efc8fc7"
+)
+
+func TestSatelitServer_AddVolume(t *testing.T) {
+	ctx, client, teardown := getSatelitClient()
+	defer teardown()
+
+	req := &pb.AddVolumeRequest{
+		Name:             testUUID,
+		CapacityGigabyte: testCapacityGigabyte,
+	}
+
+	resp, err := client.AddVolume(ctx, req)
+	if err != nil {
+		t.Errorf("AddVolume return error: %+v", err)
+	}
+
+	want := pb.Volume{
+		Id:               testUUID,
+		CapacityGigabyte: testCapacityGigabyte,
+	}
+
+	if diff := deep.Equal(resp.Volume, &want); diff != nil {
+		t.Error(diff)
 	}
 }
---
+++ pkg/ganymede/memory/memory.go
@@ -0,0 +1,41 @@
+package memory
+
+import (
+	"context"
+	"fmt"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/whywaita/satelit/pkg/datastore"
+	"github.com/whywaita/satelit/pkg/ganymede"
+)
+
+// Memory is backend of ganymede by in-memory for testing
+type Memory struct {
+	ds datastore.Datastore
+}
+
+// New create ganymade application
+func New(ds datastore.Datastore) *Memory {
+	return &Memory{ds: ds}
+}
+
+// CreateVirtualMachine add virtual machine
+func (m *Memory) CreateVirtualMachine(ctx context.Context, name string, vcpus uint32, memoryKiB uint64, bootDeviceName, hypervisorName string) (*ganymede.VirtualMachine, error) {
+	u := uuid.NewV4()
+
+	vm := &ganymede.VirtualMachine{
+		UUID:           u,
+		Name:           name,
+		Vcpus:          vcpus,
+		MemoryKiB:      memoryKiB,
+		HypervisorName: hypervisorName,
+	}
+
+	err := m.ds.PutVirtualMachine(*vm)
+	if err != nil {
+		return nil, fmt.Errorf("failed to write virtual machine: %w", err)
+	}
+
+	return vm, nil
+}
