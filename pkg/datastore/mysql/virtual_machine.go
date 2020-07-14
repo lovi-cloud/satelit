ea1b468143d2ee4c886b2a4d9f4d8fe710d05a97
whywaita <whywaita@whywrite.it>
2020-07-14 10:48:06 +0900

    delete root volume when delete virtual_machine

--- pkg/datastore/mysql/virtual_machine.go
+++ pkg/datastore/mysql/virtual_machine.go
@@ -11,7 +11,7 @@ import (
 // GetVirtualMachine return virtual machine record
 func (m *MySQL) GetVirtualMachine(vmID uuid.UUID) (*ganymede.VirtualMachine, error) {
 	var vm ganymede.VirtualMachine
-	query := fmt.Sprintf(`SELECT uuid, name, vcpus, memory_kib, hypervisor_name FROM virtual_machine WHERE uuid = '%s'`, vmID.String())
+	query := fmt.Sprintf(`SELECT uuid, name, vcpus, memory_kib, hypervisor_name, root_volume_id FROM virtual_machine WHERE uuid = '%s'`, vmID.String())
 	err := m.Conn.Get(&vm, query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to execute query: %w", err)
@@ -22,8 +22,8 @@ func (m *MySQL) GetVirtualMachine(vmID uuid.UUID) (*ganymede.VirtualMachine, err
 
 // PutVirtualMachine write virtual machine record
 func (m *MySQL) PutVirtualMachine(vm ganymede.VirtualMachine) error {
-	query := `INSERT INTO virtual_machine(name, uuid, vcpus, memory_kib, hypervisor_name) VALUES (?, ?, ?, ?, ?)`
-	_, err := m.Conn.Exec(query, vm.Name, vm.UUID, vm.Vcpus, vm.MemoryKiB, vm.HypervisorName)
+	query := `INSERT INTO virtual_machine(name, uuid, vcpus, memory_kib, hypervisor_name, root_volume_id) VALUES (?, ?, ?, ?, ?, ?)`
+	_, err := m.Conn.Exec(query, vm.Name, vm.UUID, vm.Vcpus, vm.MemoryKiB, vm.HypervisorName, vm.RootVolumeID)
 	if err != nil {
 		return fmt.Errorf("failed to execute insert query: %w", err)
 	}
